8138b8503772d5dfe16a84cab8cb9cef8c6de84a
John Barton <johnrbarton@ufl.edu>
2014-10-22 22:27:13 -0400

    Fixed issue by removing calendar controller (All "calendar" controller code is now in the main "event" controller)

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -23,9 +23,8 @@ var EventSchema = new Schema({
 		trim: true
 	},
 	date: {
-		type: Date,
-		required: 'Please include Event date',
-		trim: true
+		type: String,
+		required: 'Please include Event date'
 	},
     time: {
         type: String,
--- public/modules/events/controllers/events.client.controller.js
+++ public/modules/events/controllers/events.client.controller.js
@@ -76,5 +76,42 @@ angular.module('events').controller('EventsController', ['$scope', '$stateParams
 				eventId: $stateParams.eventId
 			});
 		};
+    
+        //Calendar Controller
+        $scope.today = function() {
+            $scope.date = new Date();
+        };
+        
+        $scope.today();
+
+        $scope.clear = function () {
+            $scope.date = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+  
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd/MMMM/yyyy', 'yyyy/MM/dd', 'dd/MM/yyyy', 'MM/dd/yyyy', 'shortDate'];
+        $scope.format = $scope.formats[3];        
 	}
 ]);
