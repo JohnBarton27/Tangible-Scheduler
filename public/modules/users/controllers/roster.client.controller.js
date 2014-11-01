c0389be292447daea9a8ff8284f790e64401809c
John Barton <johnrbarton@ufl.edu>
2014-11-01 13:45:28 -0400

    Added full "Roster" page Displays "lastName, firstName" along with Phone Number and Email

--- public/modules/users/controllers/roster.client.controller.js
+++ public/modules/users/controllers/roster.client.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('users').controller('RosterController', ['$scope', '$stateParams', '$location', 'Authentication', 'Events','Projects','Skillsets','Users',
-	function($scope) {
+angular.module('users').controller('RosterController', ['$scope', 'Users',
+	function($scope, Users) {
 		// Find a list of Users
 		$scope.findUsers = function() {
 			$scope.users = Users.query();
-		};
+		};       
 	}
 ]);
