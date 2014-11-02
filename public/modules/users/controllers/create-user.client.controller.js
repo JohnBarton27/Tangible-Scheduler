50cd59356ccafdda31a78d341aa9ed98bf86f1a2
rmatthews384 <rmatthews384@yahoo.com>
2014-11-02 14:56:56 -0500

    Redoing create-user to implement the signup - still not working though

--- public/modules/users/controllers/create-user.client.controller.js
+++ public/modules/users/controllers/create-user.client.controller.js
@@ -1,61 +1,21 @@
 'use strict';
 
-angular.module('users').controller('CreateUserController', ['$scope', '$stateParams', '$location', 'Authentication', 'Users',
-	function($scope) {
-		
-		$scope.create = function() {
+angular.module('users').controller('create-user', ['$scope', '$http', '$location', 'Authentication',
+	function($scope, $http, $location, Authentication) {
+		$scope.authentication = Authentication;
 
-			var user = new Users ({
-				firstName:   this.firstName,
-                lastName:    this.lastName,
-                phone:       this.phone,
-                email:       this.email,
-                skills:      this.skills,
-			});
 
-			// Redirect after save
-			user.$save(function(response) {
-				$location.path('users/' + response._id);
 
-				// Clear form fields
-				$scope.firstName = '';
-                $scope.lastName = '';
-                $scope.date = '';
-                $scope.phone = '';
-                $scope.email = '';
-                $scope.skills = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
+		$scope.signup = function() {
+			$http.post('/auth/signup', $scope.credentials).success(function(response) {
+				// If successful we assign the response to the global user model
+				//$scope.authentication.user = response;
+
+				// And redirect to the index page
+				//$location.path('/');
+			}).error(function(response) {
+				$scope.error = response.message;
 			});
 		};
-
-		// Remove existing user
-		$scope.remove = function( users ) {
-			if ( user ) { user.$remove();
-
-				for (var i in $scope.users ) {
-					if ($scope.users [i] === user ) {
-						$scope.users.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.user.$remove(function() {
-					$location.path('users');
-				});
-			}
-		};
-
-		// Update existing user
-		$scope.update = function() {
-			var user = $scope.user ;
-
-			user.$update(function() {
-				$location.path('users/' + user._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
-
 	}
 ]);
