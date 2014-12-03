b9825f311cd1c9f7adfd74c494017e81c1a19185
Aaron Abbott <aabmass@gmail.com>
2014-12-03 02:07:50 -0500

    Set up function to retrieve a user by id
    findUser(userId) in RosterController can be called and works with the
    route /users/:id

--- public/modules/users/controllers/roster.client.controller.js
+++ public/modules/users/controllers/roster.client.controller.js
@@ -2,10 +2,16 @@
 
 angular.module('users').controller('RosterController', ['$scope', 'Users', 'Skillsets',
 	function($scope, Users, Skillsets) {
+
 		// Find a list of Users
 		$scope.findUsers = function() {
 			$scope.users = Users.query();
 		};
+
+        // Find a user by Id. Not sure if this will block for network or not
+        $scope.findUser = function(userId) {
+            return Users.userByID({id: userId});
+        }
         
         $scope.findSkills = function() {
             $scope.skills = Skillsets.query();
