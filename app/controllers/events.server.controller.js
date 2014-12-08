2479b8f6b756897ffc183f9691c1ce202a337310
Juandi007 <Juandis_007@hotmail.com>
2014-12-08 11:09:52 -0500

    Send email per created Skill Request
    Added code in event control to send an email for each event request
    created. Code is untested. My machine is not running the website
    properly even though i have updated pulls. Will test on other computers.

--- app/controllers/events.server.controller.js
+++ app/controllers/events.server.controller.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose'),
 	Project = mongoose.model('Project'),
 	SkillRequest = mongoose.model('SkillRequest'),
 	EventRequest = mongoose.model('EventRequest'),
+	nodemailer = require('nodemailer'),
 	_ = require('lodash');
 
 /**
@@ -67,8 +68,10 @@ exports.create = function(req, res) {
 						);
 
 						//Create Event Requests for required users
+						//Case where there are no required users
 						if (srequest.requiredUsers.length == 0) {
 							var usedUsersID = [];
+							//count how many users available with requested skill
 							Users.count({'skills': srequest.skill}, function(err, c) {
 								if (err) {
 									console.log('Error');
@@ -81,25 +84,181 @@ exports.create = function(req, res) {
 											}
 											else {
 												//check if user has already been used
-												for (var k = 0; k < usedUsers.length; k++) {
+												var skip = false;
+												for (var k = 0; k < usedUsersID.length; k++) {
+													//User has already been used
 													if (usedUsersID[k] == use._id) {
 														j--;
+														skip = true;
+														break;
 													}
-													else {
-														usedUsersID[j] = use._id;
-														var EReq = new EventRequest({
-															event: newEvent,
-															user: use
-														});
-														EReq.save(function(err, newER) {
-															if (err) {
-																console.log('Error');
-															}
-															else {
+												}
 
-															}
-														});
+												if (skip) {
+													continue;
+												}
+												else {
+													usedUsersID[j] = use._id;
+													var EReq = new EventRequest({
+														event: newEvent,
+														user: use
+													});
+													EReq.save(function(err, newER) {
+														if (err) {
+															console.log("Error");
+														}
+														else {
+															var transporter = nodemailer.createTransport({
+															    service: 'Gmail',
+															    auth: {
+															        user: 'tangibletesting@gmail.com',
+															        pass: '$tangibletesting123'
+															    }
+															});
+
+															var msgtext = 'You '.concat('are requested to appear
+																at ', newEvent.name, '. Please respond to this email
+																with your attending status.');
+															var mailOptions = {
+															    from: 'tangibletesting@gmail.com',
+															    to: newER.user.email,
+															    subject: 'Event Request',
+															    text: msgtext
+															};
+
+															transporter.sendMail(mailOptions, function(err, info) {
+															    if (err) {
+															           console.log(err);
+															    }
+															    else {
+															          console.log('Message send: ' + info.response);
+															    }
+															});
+														}
+													});
+												}
+											}
+										});
+									}
+								}
+							});
+						}
+						//Case where there are requried users
+						else {
+							//Make and mail each event request for the required users
+							var usedUsersID = [];
+							for (var j = 0; j < srequest.requiredUsers.length; j++) {
+								Users.findById(srequest.requiredUsers[j]._id, function(err, fuser) {
+									if (err) {
+										console.log('Error');
+									}
+									else {
+										usedUsersID[usedUsersID.length] = fuser._id;
+										var EReq = new EventRequest({
+											event: newEvent,
+											user: fuser
+										});
+										EReq.save(function(err, newER) {
+											if (err) {
+												console.log("Error");
+											}
+											else {
+												var transporter = nodemailer.createTransport({
+												    service: 'Gmail',
+												    auth: {
+												        user: 'tangibletesting@gmail.com',
+												        pass: 'tangibletesting123'
+												    }
+												});
+
+												var msgtext = 'You '.concat('are requested to appear
+													at ', newEvent.name, '. Please respond to this email
+													with your attending status.');
+												var mailOptions = {
+												    from: 'tangibletesting@gmail.com',
+												    to: newER.user.email,
+												    subject: 'Event Request',
+												    text: msgtext
+												};
+
+												transporter.sendMail(mailOptions, function(err, info) {
+												    if (err) {
+												           console.log(err);
+												    }
+												    else {
+												          console.log('Message send: ' + info.response);
+												    }
+												});
+											}
+										});
+									}
+								})
+							}
+							//now go back and do the random amount of users left
+							Users.count({'skills': srequest.skill}, function(err, c) {
+								if (err) {
+									console.log('Error');
+								}
+								else {
+									for (var j = 0; j < srequest.numRequested - usedUsersID.length; j++) {
+										Users.findOne({'skills': srequest.skill}, null, {skip: Math.floor(Math.random()*c)}, function(err, use) {
+											if (err) {
+												console.log('Error');
+											}
+											else {
+												//check if user has already been used
+												var skip = false;
+												for (var k = 0; k < usedUsersID.length; k++) {
+													//User has already been used
+													if (usedUsersID[k] == use._id) {
+														j--;
+														skip = true;
+														break;
 													}
+												}
+
+												if (skip) {
+													continue;
+												}
+												else {
+													usedUsersID[usedUsersID.length] = use._id;
+													var EReq = new EventRequest({
+														event: newEvent,
+														user: use
+													});
+													EReq.save(function(err, newER) {
+														if (err) {
+															console.log("Error");
+														}
+														else {
+															var transporter = nodemailer.createTransport({
+															    service: 'Gmail',
+															    auth: {
+															        user: 'tangibletesting@gmail.com',
+															        pass: 'tangibletesting123'
+															    }
+															});
+
+															var msgtext = 'You '.concat('are requested to appear
+																at ', newEvent.name, '. Please respond to this email
+																with your attending status.');
+															var mailOptions = {
+															    from: 'tangibletesting@gmail.com',
+															    to: newER.user.email,
+															    subject: 'Event Request',
+															    text: msgtext
+															};
+
+															transporter.sendMail(mailOptions, function(err, info) {
+															    if (err) {
+															           console.log(err);
+															    }
+															    else {
+															          console.log('Message send: ' + info.response);
+															    }
+															});
+														}
+													});
 												}
 											}
 										});
