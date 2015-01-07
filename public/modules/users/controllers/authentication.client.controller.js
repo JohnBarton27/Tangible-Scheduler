d31d7f17274b8591c752a755ee71b2ae5e6684f8
John Barton <johnrbarton@ufl.edu>
2015-01-07 17:26:12 -0500

    Made it so the "Add Skill" button on Skillset views clicks through to that user's "Edit User" page

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -11,7 +11,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
-
+                
 				// And redirect to the index page
 				$location.path('/');
 			}).error(function(response) {
